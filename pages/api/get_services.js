c8b963333c5d76e69492fc1a7e7921eba34dcd36
mkJou <joalexint@gmail.com>
2023-05-24 13:23:57 -0400

    get webpage service

--- pages/api/get_services.js
+++ pages/api/get_services.js
@@ -43,72 +43,89 @@ export default async function handler(req, res) {
     let params = [];
     params.push(inParams);
 
-    if (p.name) {
-      odoo.execute_kw("x_services", "search_read", params, (err, value) => {
-        if (err) {
-          console.log("ERROR", err);
-          return res.json({
-            error: err,
-          });
+    if (p.id) {
+      let inParams2 = [];
+      inParams2.push([["id", "=", Number(p.id)]]);
+      inParams2.push([
+        "id",
+        "website_url",
+        "url",
+        "arch",
+        "__last_update",
+        "display_name",
+        "website_meta_title",
+        "website_meta_description",
+        "website_meta_keywords",
+        "website_meta_og_img",
+      ]);
+
+      let params2 = [];
+      params2.push(inParams2);
+
+      odoo.execute_kw(
+        "website.page",
+        "search_read",
+        params2,
+        async (err2, value2) => {
+          if (err2) {
+            console.log("ERROR", err2);
+            return res.json({
+              error: err2,
+            });
+          }
+
+          if (!value2) return res.json({});
+
+          return res.json(value2[0]);
+
+          // let inParams3 = [];
+          // inParams3.push([["x_services_id", "=", theService.id]]);
+
+          // let params3 = [];
+          // params3.push(inParams3);
+
+          // odoo.execute_kw(
+          //   "x_services_line_96e9a",
+          //   "search_read",
+          //   params3,
+          //   async (err3, value3) => {
+          //     if (err3) return console.log("ERROR:", err3);
+
+          //     return res.json({
+          //       service: theService,
+          //       website_page: value2[0],
+          //       faq: value3,
+          //     });
+          //   }
+          // );
         }
+      );
 
-        if (value.length < 1) return res.json([]);
+      // odoo.execute_kw("x_services", "search_read", params, (err, value) => {
+      //   if (err) {
+      //     console.log("ERROR", err);
+      //     return res.json({
+      //       error: err,
+      //     });
+      //   }
 
-        let searchService = "";
-        let theService = {};
+      //   if (value.length < 1) return res.json([]);
 
-        value.forEach((service) => {
-          if (
-            String(service.x_name).replace(/\ /g, "-").toLowerCase() == p.name
-          ) {
-            theService = service;
-            searchService = service.x_studio_associated_page[0];
-          }
-        });
+      //   let searchService = "";
+      //   let theService = {};
 
-        if (!searchService) return res.json([]);
+      //   value.forEach((service) => {
+      //     if (
+      //       String(service.x_name).replace(/\ /g, "-").toLowerCase() == p.name
+      //     ) {
+      //       theService = service;
+      //       searchService = service.x_studio_associated_page[0];
+      //     }
+      //   });
 
-        let inParams2 = [];
-        inParams2.push([["id", "=", Number(searchService)]]);
+      //   if (!searchService) return res.json([]);
 
-        let params2 = [];
-        params2.push(inParams2);
-
-        odoo.execute_kw(
-          "website.page",
-          "search_read",
-          params2,
-          async (err2, value2) => {
-            if (err2) {
-              console.log("ERROR", err2);
-              return res.json({
-                error: err2,
-              });
-            }
-
-            let inParams3 = [];
-            inParams3.push([["x_services_id", "=", theService.id]]);
-
-            let params3 = [];
-            params3.push(inParams3);
-
-            odoo.execute_kw(
-              "x_services_line_96e9a",
-              "search_read",
-              params3,
-              async (err3, value3) => {
-                if (err3) return console.log("ERROR:", err3);
-
-                return res.json({
-                  service: theService,
-                  website_page: value2[0],
-                  faq: value3,
-                });
-              }
-            );
-          }
-        );
-      });
+      // });
     } else {
       odoo.execute_kw("x_services", "search_read", params, (err, value) => {
         if (err) return console.log("ERROR:", err);
