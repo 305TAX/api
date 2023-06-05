9a40fea2cb917b68a043e8666bfb929c4b2b86d2
mkJou <joalexint@gmail.com>
2023-06-05 15:29:53 -0400

    5 6 2023

--- pages/api/testing.js
+++ pages/api/testing.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../lib/mongodb";
+//import clientPromise from "../../lib/mongodb";
 
 import NextCors from "nextjs-cors";
 
@@ -10,12 +10,16 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  const customer = "Reinaldo Armas";
-  let linkRedirect = "https://305tax.com/";
+  // const customer = "Reinaldo Armas";
+  // let linkRedirect = "https://305tax.com/";
 
-  const result = encodeURI(
-    `Estimado, ${customer}.\n\nLe damos las gracias por elegir a 305TAX como sus asesores tributarios y por permitirnos ayudarle con sus necesidades fiscales. Esperamos que haya quedado satisfecho con el nivel de servicio que le hemos proporcionado y que su experiencia haya sido positiva.\n\nComo pequeña empresa, dependemos en gran medida de las opiniones positivas y de las recomendaciones de boca en boca. Estaríamos muy agradecidos si pudiera dedicar unos minutos a dejarnos su opinión sobre nuestros servicios.\n\nHaz click en el siguiente enlace:\n${linkRedirect}`
-  );
+  // const result = encodeURI(
+  //   `Estimado, ${customer}.\n\nLe damos las gracias por elegir a 305TAX como sus asesores tributarios y por permitirnos ayudarle con sus necesidades fiscales. Esperamos que haya quedado satisfecho con el nivel de servicio que le hemos proporcionado y que su experiencia haya sido positiva.\n\nComo pequeña empresa, dependemos en gran medida de las opiniones positivas y de las recomendaciones de boca en boca. Estaríamos muy agradecidos si pudiera dedicar unos minutos a dejarnos su opinión sobre nuestros servicios.\n\nHaz click en el siguiente enlace:\n${linkRedirect}`
+  // );
 
-  return res.send(result);
+  console.log("SE ENVIO", req.query)
+
+  return res.json({
+    reuslt: "listo"
+  })
 }
---
+++ pages/api/webhook_monday.js
@@ -0,0 +1,51 @@
+//import clientPromise from "../../lib/mongodb";
+
+//import Odoo from "odoo-xmlrpc";
+
+import NextCors from "nextjs-cors";
+//import { odooConfig } from "../../lib/odooConfig";
+
+export default async function handler(req, res) {
+  //NEXT CORS
+  await NextCors(req, res, {
+    methods: ["GET", "POST"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+  //QUERY
+  const p = req.query;
+
+  const capitalized = (word) => {
+    let wordSplit = String(word).split(" ");
+    let result = "";
+    for (let index = 0; index < wordSplit.length; index++) {
+      const element = wordSplit[index];
+      result = result.concat(
+        element.charAt(0).toUpperCase() + element.slice(1) + " "
+      );
+    }
+
+    return result;
+  };
+
+  const queryArray = "query { boards (ids:4589083677) { name columns {id title} }}"
+
+  const resp = await fetch("https://api.monday.com/v2", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI2MDIyMDgwMiwiYWFpIjoxMSwidWlkIjozNzE3MzE0OCwiaWFkIjoiMjAyMy0wNi0wMlQxNToxNjo0Ni4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQzOTMxNzMsInJnbiI6InVzZTEifQ.DKOZtmfsOv5OC6DDUwfMiI8fGdx3VOkZks3OmVHINRA",
+      "API-Version": "2023-04",
+    },
+    body: JSON.stringify({
+      query: "mutation { create_item (board_id: 4589083677, item_name: \"afd\", column_values: \"{\\\"date4\\\":\\\"2023-05-25\\\", \\\"status\\\":\\\"2\\\"}\") { id name }}",
+    }),
+  });
+  const mamit = await resp.json();
+
+  return res.json({
+    res: mamit,
+  });
+}
