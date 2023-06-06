8c787185181dae7ab3ee1d8468259e9ed44e9d03
mkJou <joalexint@gmail.com>
2023-06-06 12:24:01 -0400

    fix Object Body

--- pages/api/testing.js
+++ pages/api/testing.js
@@ -98,7 +98,7 @@ export default async function handler(req, res) {
   //   `Estimado, ${customer}.\n\nLe damos las gracias por elegir a 305TAX como sus asesores tributarios y por permitirnos ayudarle con sus necesidades fiscales. Esperamos que haya quedado satisfecho con el nivel de servicio que le hemos proporcionado y que su experiencia haya sido positiva.\n\nComo pequeña empresa, dependemos en gran medida de las opiniones positivas y de las recomendaciones de boca en boca. Estaríamos muy agradecidos si pudiera dedicar unos minutos a dejarnos su opinión sobre nuestros servicios.\n\nHaz click en el siguiente enlace:\n${linkRedirect}`
   // );
 
-  const arrBody = req.body;
+  const arrBody = JSON.parse(JSON.stringify(req.body));
   console.log("SE ENVIO", req.query, "BODY", arrBody);
 
   return res.json({
