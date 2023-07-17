2475a52cb782bd8a03834a4bdf9d8f8b7779c567
mkJou <joalexint@gmail.com>
2023-07-17 14:25:47 -0400

    up

--- pages/api/defaultWaMessage.js
+++ pages/api/defaultWaMessage.js
@@ -24,5 +24,32 @@ export default async function handler(req, res) {
     res.status(200).json({
       result: objectData,
     });
+  } else if (req.method === "POST") {
+    try {
+      // Read the existing data from the JSON file
+      const jsonData = await fsPromises.readFile(dataFilePath);
+      const objectData = JSON.parse(jsonData);
+
+      // Get the data from the request body
+      const { msgdf } = req.query;
+
+      // Add the new data to the object
+      const newData = {
+        messageDefault: msgdf,
+      };
+
+      // Convert the object back to a JSON string
+      const updatedData = JSON.stringify(newData);
+
+      // Write the updated data to the JSON file
+      await fsPromises.writeFile(dataFilePath, updatedData);
+
+      // Send a success response
+      res.status(200).json({ message: "Data stored successfully" });
+    } catch (error) {
+      console.error(error);
+      // Send an error response
+      res.status(500).json({ message: "Error storing data" });
+    }
   }
 }
