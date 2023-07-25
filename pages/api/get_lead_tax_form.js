28397a7ce7cfc2cca366f1f028744e817bb3cf55
mkJou <joalexint@gmail.com>
2023-07-25 10:27:45 -0400

    upe

---
+++ pages/api/get_lead_tax_form.js
@@ -0,0 +1,57 @@
+import clientPromise from "../../lib/mongodb";
+import Odoo from "odoo-xmlrpc";
+
+import NextCors from "nextjs-cors";
+import { odooConfig } from "../../lib/odooConfig";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "POST"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  const q = req.query;
+
+  let odoo = new Odoo(odooConfig);
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("users_reviews");
+
+    const response = await db.collection("forms_tax").find({}).toArray();
+
+    const findResponse = response?.filter(
+      (user) => user.info_lead.odoo_id == q?.id
+    )[0];
+    console.log("response api", findResponse);
+
+    if (findResponse == undefined) {
+      return res.json({
+        result: false,
+      });
+    } else {
+      return res.json(
+        response?.filter((user) => user.info_lead.odoo_id == q?.id)[0]
+      );
+    }
+
+    // if (response) {
+    //   const findUser = response.result;
+    //   // const result = findUser.find(
+    //   //   (user, index) => user.info_lead.odoo_id == q?.id
+    //   // );
+
+    //   return res.json({
+    //     result: findUser,
+    //   });
+  } catch (error) {
+    return res.json({
+      result: false,
+      error: error,
+    });
+  }
+}
---
+++ pages/api/updateTaxForm.js
@@ -0,0 +1,62 @@
+import clientPromise from "../../lib/mongodb";
+import Odoo from "odoo-xmlrpc";
+
+import NextCors from "nextjs-cors";
+import { odooConfig } from "../../lib/odooConfig";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "POST"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  const { notCrmId, info_lead, crm_id, form_modify, form_our } = JSON.parse(
+    req.body
+  );
+
+  try {
+    // return res.json({
+    //   form_modify: form_modify,
+    //   form_our: form_our,
+    // });
+    const client = await clientPromise;
+    const db = client.db("users_reviews");
+
+    if (notCrmId == true) {
+      const response = await db.collection("forms_tax").insertOne({
+        info_lead: info_lead,
+        form_modify: form_modify,
+        form_our: form_our,
+      });
+      return res.json({
+        result: true,
+      });
+      console.log(response, notCrmId, "NOT CRM ID");
+    } else {
+      const response = await db.collection("forms").updateOne(
+        { "info_lead.crm_id": crm_id },
+        {
+          $set: {
+            "info_lead.update_time": new Date(),
+            form_modify: form_modify,
+            form_our: form_our,
+          },
+        }
+      );
+      return res.json({
+        result: true,
+      });
+      console.log(response, crm_id, form_modify, form_our);
+    }
+
+    // if (response) {
+    //   return true;
+    // }
+  } catch (error) {
+    console.log("ERRO MONGODB", error);
+  }
+}
