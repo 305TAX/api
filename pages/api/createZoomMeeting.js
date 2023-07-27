cc3ad562d73fe4409c631da85989ace7cb7ca38d
mkJou <joalexint@gmail.com>
2023-07-27 10:59:26 -0400

    up

---
+++ pages/api/createZoomMeeting.js
@@ -0,0 +1,127 @@
+//import clientPromise from "../../lib/mongodb";
+import Odoo from "odoo-xmlrpc";
+
+import NextCors from "nextjs-cors";
+import { odooConfig } from "../../lib/odooConfig";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "POST"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  //ODOO CONFIGURATION
+  let odoo = new Odoo(odooConfig);
+
+  const query = req.query;
+
+  if (query?.event_id) {
+    return res.json({
+      result: "event",
+    });
+  } else if (query?.success) {
+    console.log("REUSLT SUSCCESS", query);
+    return res.json({
+      result: query,
+    });
+  } else {
+    return res.json({
+      result: false,
+    });
+  }
+
+  // odoo.connect(function (err) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+
+  //   console.log("Connected to Odoo server.");
+
+  //   var inParams = [];
+  //   inParams.push([["id", "=", Number(query?.event_id)]]);
+  //   inParams.push(["name", "start", "duration"]); //fields
+  //   inParams.push(0); //offset
+  //   inParams.push(5); //limit
+  //   var params = [];
+  //   params.push(inParams);
+  //   odoo.execute_kw(
+  //     "calendar.event",
+  //     "search_read",
+  //     params,
+  //     async function (err, value) {
+  //       if (err) {
+  //         return console.log(err);
+  //       }
+
+  //       const result = value[0];
+  //       const time = Number((60 * Number(value[0].duration)).toFixed(0));
+  //       result.duration = time;
+
+  //       try {
+  //         const fetchingHook = await fetch(
+  //           "https://hooks.zapier.com/hooks/catch/15998712/31tc0ed/",
+  //           {
+  //             method: "POST",
+  //             body: JSON.stringify(result),
+  //             headers: {
+  //               "Content-Type": "application/json",
+  //             },
+  //           }
+  //         );
+
+  //         const resultFetch = await fetchingHook.json();
+
+  //         return res.json({
+  //           state: true,
+  //           result_zoom: resultFetch,
+  //         });
+  //       } catch (error) {
+  //         return res.json({
+  //           state: false,
+  //           error: error,
+  //         });
+  //       }
+  //     }
+  //   );
+  // });
+
+  // const query = JSON.parse(JSON.stringify(req.body));
+  //   const query = {
+  //     companyname: "Brandon Test",
+  //     email: "joalexint@gmail.com",
+  //     displayname: "Brandon Test",
+  //     familyname: "Brandon Test",
+  //     phonenumber: "+584246002286",
+  //     givenname: "Brandon Test",
+  //   };
+
+  // const newContact = {
+  //   givenName: String(query?.displayname),
+  //   email: String(query?.email),
+  //   mobile: String(query?.phonenumber),
+  // };
+
+  // CREATE GOOGLE CONTACT
+
+  // const jsonQuery = new URLSearchParams(newContact).toString();
+  // const linkResponse = `${process.env.QB_API}/create_google_contact?${jsonQuery}`;
+
+  // const response = await fetch(linkResponse, {
+  //   method: "POST",
+  // });
+  // const result = await response.json();
+
+  // console.log("RESULT", linkResponse, result);
+
+  //   const queryStringify = JSON.stringify(query);
+  //   console.log("REQ BODY", queryStringify);
+  //   let xds = String(process.env.QB_API);
+
+  // return res.json({
+  //   result: result,
+  // });
+}
--- pages/api/testing.js
+++ pages/api/testing.js
@@ -1,7 +1,9 @@
-import clientPromise from "../../lib/mongodb";
+// import clientPromise from "../../lib/mongodb";
 
+import Odoo from "odoo-xmlrpc";
 import NextCors from "nextjs-cors";
 import unirest from "unirest";
+import { odooConfig } from "../../lib/odooConfig";
 import cheerio from "cheerio";
 
 export default async function handler(req, res) {
@@ -12,41 +14,54 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  let url = "https://joalexurdaneta.com";
+  let odoo = new Odoo(odooConfig);
 
-  async function replaceUrlsAndShorten(text) {
-    const urlRegex = /https?:\/\/[^\s]+/g;
-    let newText = text;
-    var match = "";
-    while ((match = urlRegex.exec(text)) !== null) {
-      try {
-        let url = String(match[0]);
-        const response = await fetch("https://review.305tax.com/api/shorten", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ url }),
-        })
-          .then((res) => res.json())
-          .then((result) => {
-            newText = newText.replace(
-              match,
-              `https://305tax.com/l/${result.key}`
-            );
-          });
-      } catch (error) {
-        console.log("Error", error);
-      }
+  odoo.connect((err) => {
+    if (err) {
+      return console.log(err);
     }
-    return newText;
-  }
+    console.log("Connected to Odoo server.");
+    var inParams = [];
+    inParams.push([["stage_id", "=", 1]]);
+    inParams.push(["name", "mobile", "email_from"]);
+    var params = [];
+    params.push(inParams);
 
-  const ts = await replaceUrlsAndShorten(
-    "Hello dear https://google.com/ sin embargo si quieren ir a https://facebook.com/utm/peper/"
-  );
+    // 4- Read
+    odoo.execute_kw("crm.lead", "search_read", params, async (err, value) => {
+      if (err) return console.log("ERROR:", err);
 
-  return res.json({
-    result: ts,
+      const result = value;
+
+      await Promise.all(
+        result.map(async (val, index) => {
+          try {
+            const newContact = {
+              givenName: String(val?.name),
+              email: String(val?.email_from),
+              mobile: String(val?.mobile),
+            };
+
+            //CREATE GOOGLE CONTACT
+
+            const jsonQuery = new URLSearchParams(newContact).toString();
+
+            const linkResponse = `http://localhost:3002/create_google_contact?${jsonQuery}`;
+
+            const response = await fetch(linkResponse, {
+              method: "POST",
+            });
+            const result = await response.json();
+            console.log("RESULTADO", true);
+          } catch (error) {
+            console.log(error);
+          }
+        })
+      );
+
+      return res.json({
+        result: result,
+      });
+    });
   });
 }
