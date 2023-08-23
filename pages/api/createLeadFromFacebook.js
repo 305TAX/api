b4281b44ada38935efe9bc6c77c6ad62ac6e1f4d
mkJou <joalexint@gmail.com>
2023-08-23 10:42:59 -0400

    rp mago

--- pages/api/createLeadFromFacebook.js
+++ pages/api/createLeadFromFacebook.js
@@ -17,77 +17,163 @@ export default async function handler(req, res) {
   //ODOO CONFIGURATION
   let odoo = new Odoo(odooConfig);
   const query = req.body;
-  //CREATE PARTNER
 
-  odoo.connect(function (err) {
-    if (err) {
-      return console.log(err);
-    }
+  let isFragmentExist =
+    Object.keys(query).filter((fs) => String(fs) === "fragment").length >= 1
+      ? true
+      : false;
 
-    let inParams = [];
-    inParams.push({
-      name: String(query?.full_name),
-      email: String(query?.email),
-      mobile: String(query?.mobile),
-
-      x_studio_ad_id: String(query?.ad_id),
-      x_studio_ad_name: String(query?.ad_name),
-      x_studio_adset_id: String(query?.adset_id),
-      x_studio_adset_name: String(query?.adset_name),
-      x_studio_ad_campaign_id: String(query?.campaign_id),
-      x_studio_ad_campaign_name: String(query?.campaign_name),
-      x_studio_ad_created_time: String(query?.created_time),
-      x_studio_ad_form_id: String(query?.form_id),
-      x_studio_ad_form_name_1: String(query?.form_name),
-      x_studio_ad_lead_id: String(query?.id),
-      x_studio_ad_is_organic: String(query?.is_organic),
-      x_studio_ad_page_id: String(query?.page_id),
-      x_studio_ad_page_name: String(query?.page_name),
-      x_studio_ad_platform: String(query?.platform),
-    });
-
-    let params = [];
-    params.push(inParams);
-
-    odoo.execute_kw("res.partner", "create", params, function (err, value) {
+  if (isFragmentExist) {
+    odoo.connect(function (err) {
       if (err) {
         return console.log(err);
       }
-      console.log("Result: ", value);
 
-      if (value) {
-        odoo.connect(function (err2) {
-          if (err2) {
-            return console.log(err2);
-          }
+      let inParams = [];
+      inParams.push({
+        name: String(query?.full_name),
+        email: String(query?.email),
+        mobile: String(query?.mobile),
+        x_studio_isrosafrancia: true,
 
-          let inParams2 = [];
-          inParams2.push({
-            partner_id: value,
-            name: String(query?.full_name).toUpperCase(),
-            email_from: String(query?.email),
-            user_id: "",
-          });
+        x_studio_ad_id: String(query?.ad_id),
+        x_studio_ad_name: String(query?.ad_name),
+        x_studio_adset_id: String(query?.adset_id),
+        x_studio_adset_name: String(query?.adset_name),
+        x_studio_ad_campaign_id: String(query?.campaign_id),
+        x_studio_ad_campaign_name: String(query?.campaign_name),
+        x_studio_ad_created_time: String(query?.created_time),
+        x_studio_ad_form_id: String(query?.form_id),
+        x_studio_ad_form_name_1: String(query?.form_name),
+        x_studio_ad_lead_id: String(query?.id),
+        x_studio_ad_is_organic: String(query?.is_organic),
+        x_studio_ad_page_id: String(query?.page_id),
+        x_studio_ad_page_name: String(query?.page_name),
+        x_studio_ad_platform: String(query?.platform),
+      });
 
-          let params2 = [];
-          params2.push(inParams2);
+      let params = [];
+      params.push(inParams);
 
-          odoo.execute_kw(
-            "crm.lead",
-            "create",
-            params2,
-            function (err2, value2) {
-              if (err2) {
-                return console.log(err2);
-              }
-              console.log("Result: ", value2);
-              return res.json({
-                result: [value, value2],
-              });
+      odoo.execute_kw("res.partner", "create", params, function (err, value) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Result: ", value);
+
+        if (value) {
+          odoo.connect(function (err2) {
+            if (err2) {
+              return console.log(err2);
             }
-          );
-        });
-      }
+
+            let inParams2 = [];
+            inParams2.push({
+              partner_id: value,
+              name: String(query?.full_name).toUpperCase(),
+              email_from: String(query?.email),
+              user_id: "",
+            });
+
+            let params2 = [];
+            params2.push(inParams2);
+
+            odoo.execute_kw(
+              "crm.lead",
+              "create",
+              params2,
+              function (err2, value2) {
+                if (err2) {
+                  return console.log(err2);
+                }
+                console.log("Result: ", value2);
+                return res.json({
+                  result: [value, value2],
+                });
+              }
+            );
+          });
+        }
+      });
     });
-  });
+  } else {
+    odoo.connect(function (err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      let inParams = [];
+      inParams.push({
+        name: String(query?.full_name),
+        email: String(query?.email),
+        mobile: String(query?.mobile),
+
+        x_studio_ad_id: String(query?.ad_id),
+        x_studio_ad_name: String(query?.ad_name),
+        x_studio_adset_id: String(query?.adset_id),
+        x_studio_adset_name: String(query?.adset_name),
+        x_studio_ad_campaign_id: String(query?.campaign_id),
+        x_studio_ad_campaign_name: String(query?.campaign_name),
+        x_studio_ad_created_time: String(query?.created_time),
+        x_studio_ad_form_id: String(query?.form_id),
+        x_studio_ad_form_name_1: String(query?.form_name),
+        x_studio_ad_lead_id: String(query?.id),
+        x_studio_ad_is_organic: String(query?.is_organic),
+        x_studio_ad_page_id: String(query?.page_id),
+        x_studio_ad_page_name: String(query?.page_name),
+        x_studio_ad_platform: String(query?.platform),
+      });
+
+      let params = [];
+      params.push(inParams);
+
+      odoo.execute_kw("res.partner", "create", params, function (err, value) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Result: ", value);
+
+        if (value) {
+          odoo.connect(function (err2) {
+            if (err2) {
+              return console.log(err2);
+            }
+
+            let inParams2 = [];
+            inParams2.push({
+              partner_id: value,
+              name: String(query?.full_name).toUpperCase(),
+              email_from: String(query?.email),
+              user_id: "",
+            });
+
+            let params2 = [];
+            params2.push(inParams2);
+
+            odoo.execute_kw(
+              "crm.lead",
+              "create",
+              params2,
+              function (err2, value2) {
+                if (err2) {
+                  return console.log(err2);
+                }
+                console.log("Result: ", value2);
+                return res.json({
+                  result: [value, value2],
+                });
+              }
+            );
+          });
+        }
+      });
+    });
+  }
+
+  // return res.json({
+  //   result: isFragmentExist,
+  //   ls: query?.fragment,
+  // })
+
+  //CREATE PARTNER
 }
