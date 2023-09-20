ad3f4725a4c69ad6646cbffd777b8fd578369a05
mkJou <joalexint@gmail.com>
2023-09-20 11:18:42 -0400

    remove query

--- pages/api/crm/sendNewLead.js
+++ pages/api/crm/sendNewLead.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
         x_studio_isrosafrancia: isRosafrancia,
         x_studio_referred_by_many: referredBy,
         x_studio_contact_origin: String(q?.contact_origin),
-        category_id: q?.categoryId ? [...q?.categoryId] : false,
+        category_id: q?.categoryId ? [q?.categoryId] : false,
       });
 
       let params = [];
