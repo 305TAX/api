a6429420fea5f7c03b0a14bd7060b27389faf4f0
mkJou <joalexint@gmail.com>
2023-10-05 16:43:45 -0400

    up

--- pages/api/crm/sendNewLead.js
+++ pages/api/crm/sendNewLead.js
@@ -21,89 +21,102 @@ export default async function handler(req, res) {
   let referredBy = q?.referredBy ? q?.referredBy : false;
   let catid = [];
 
-  q?.categoryId ? catid.push(Number(q?.categoryId)) : false;
+  q?.categoryId
+    ? String(q?.categoryId)
+        .split(",")
+        .forEach((cid, index) => {
+          catid.push(Number(cid));
+        })
+    : false;
 
-  const createNewLeadCRM = async (data) => {
-    return await new Promise((resolve, reject) => {
-      odoo.connect(function (err) {
-        if (err) {
-          return console.log(err);
-        }
+  console.log("ID", catid, q);
 
-        let inParams = [];
-
-        inParams.push({
-          partner_id: data,
-          name: String(q?.name).toUpperCase(),
-          email_from: String(q?.email),
-          user_id: "",
-        });
-
-        let params = [];
-        params.push(inParams);
-
-        odoo.execute_kw("crm.lead", "create", params, function (err2, value2) {
-          if (err2) {
-            return console.log(err2);
-          }
-
-          return resolve({
-            state: true,
-            result: value2,
-          });
-        });
-      });
-    });
-  };
-
-  const createNewPartner = new Promise(async (resolve, reject) => {
-    odoo.connect(async function (err) {
-      if (err) {
-        return console.log(err);
-      }
-
-      let inParams = [];
-      inParams.push({
-        name: String(q?.name),
-        email: String(q?.email),
-        mobile: String(q?.mobile),
-        x_studio_country_origin: String(q?.country_origin),
-        x_studio_status_verify: String(1),
-        x_studio_isrosafrancia: isRosafrancia,
-        x_studio_referred_by_many: referredBy,
-        x_studio_contact_origin: String(q?.contact_origin),
-        category_id: catid,
-      });
-
-      let params = [];
-      params.push(inParams);
-
-      odoo.execute_kw(
-        "res.partner",
-        "create",
-        params,
-        async function (err, value) {
-          if (err) {
-            return console.log(err);
-          }
-
-          console.log("Result: ", value);
-
-          if (value) {
-            const crt = await createNewLeadCRM(value);
-
-            if (crt?.state == true) {
-              return resolve({
-                status: true,
-                result: [value, crt?.result],
-              });
-            }
-          }
-        }
-      );
-    });
+  return res.status(200).json({
+    result: "listo",
   });
 
-  const responseOne = await createNewPartner;
-  return res.status(200).json(responseOne);
+  // const createNewLeadCRM = async (data) => {
+  //   return await new Promise((resolve, reject) => {
+  //     odoo.connect(function (err) {
+  //       if (err) {
+  //         return console.log(err);
+  //       }
+
+  //       let inParams = [];
+
+  //       inParams.push({
+  //         partner_id: data,
+  //         name: String(q?.name).toUpperCase(),
+  //         email_from: String(q?.email),
+  //         user_id: "",
+  //       });
+
+  //       let params = [];
+  //       params.push(inParams);
+
+  //       odoo.execute_kw("crm.lead", "create", params, function (err2, value2) {
+  //         if (err2) {
+  //           return console.log(err2);
+  //         }
+
+  //         return resolve({
+  //           state: true,
+  //           result: value2,
+  //         });
+  //       });
+  //     });
+  //   });
+  // };
+
+  // const createNewPartner = new Promise(async (resolve, reject) => {
+  //   odoo.connect(async function (err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+
+  //     let inParams = [];
+  //     inParams.push({
+  //       name: String(q?.name),
+  //       email: String(q?.email),
+  //       mobile: String(q?.mobile),
+  //       x_studio_country_origin: String(q?.country_origin),
+  //       x_studio_status_verify: String(1),
+  //       x_studio_isrosafrancia: isRosafrancia,
+  //       x_studio_referred_by_many: referredBy,
+  //       x_studio_contact_origin: String(q?.contact_origin),
+  //       x_studio_zoom_url: String(q?.studio_zoom_url),
+  //       category_id: catid,
+  //     });
+
+  //     let params = [];
+  //     params.push(inParams);
+
+  //     odoo.execute_kw(
+  //       "res.partner",
+  //       "create",
+  //       params,
+  //       async function (err, value) {
+  //         if (err) {
+  //           return console.log(err);
+  //         }
+
+  //         console.log("Result: ", value);
+
+  //         if (value) {
+  //           const crt = await createNewLeadCRM(value);
+
+  //           if (crt?.state == true) {
+  //             return resolve({
+  //               status: true,
+  //               result: [value, crt?.result],
+  //             });
+  //           }
+  //         }
+  //       }
+  //     );
+  //   });
+  // });
+
+  // const responseOne = await createNewPartner;
+  // return res.status(200).json(responseOne);
 }
