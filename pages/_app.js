2220e781e976c73657e080b357460f8473419b1e
mkJou <joalexint@gmail.com>
2024-03-14 02:48:29 -0400

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 //import React from "react";
 
-import "../styles/globals.css";
+// import "../styles/globals.css";
 // import { ThemeProvider } from "@material-tailwind/react";
 //import { ThemeProvider } from "@material-tailwind/react";
 //import Navbar from "@/components/Navbar";
